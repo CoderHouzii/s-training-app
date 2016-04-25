f5bf90c6e550063162dbf37481fb193ae155d12b
razerdp <razerdp123@gmail.com>
2016-04-25 21:18:51 +0800

    // socket心跳修复

--- sTraing/app/src/main/java/songming/straing/app/socket/SocketHandler.java
+++ sTraing/app/src/main/java/songming/straing/app/socket/SocketHandler.java
@@ -15,19 +15,19 @@ public class SocketHandler extends SimpleChannelInboundHandler<SocketMessage> {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         EventBus.getDefault().post(new Events.ConnectEvent());
-        KLog.d(TAG,"成功连接到: " + ctx.channel().remoteAddress());
+        KLog.d(TAG, "成功连接到: " + ctx.channel().remoteAddress());
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         EventBus.getDefault().post(new Events.DisconnectEvent());
-        KLog.d(TAG,"断开了连接：" + ctx.channel().remoteAddress());
+        KLog.d(TAG, "断开了连接：" + ctx.channel().remoteAddress());
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx,cause);
-        KLog.e(TAG,"服务器异常" + ctx.channel().remoteAddress());
+        super.exceptionCaught(ctx, cause);
+        KLog.e(TAG, "服务器异常" + ctx.channel().remoteAddress());
         ctx.close();
     }
 
@@ -38,11 +38,14 @@ public class SocketHandler extends SimpleChannelInboundHandler<SocketMessage> {
 
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, SocketMessage msg) throws Exception {
-        KLog.d("socket","收到消息");
-        if (msg!=null){
-            KLog.d("socket_message","msgId = "+msg.getMessageid()+"     msg = "+msg.getJsonString());
+        KLog.d("socket", "收到消息");
+        if (msg != null) {
+            KLog.d("socket_message", "msgId = " + msg.getMessageid() + "     msg = " + msg.getJsonString());
+            if (msg.getMessageid() == MessageId.SOCKET_HEART_SUCCESS) {
+                KLog.d("socket", "心跳正常");
+            }
         }
+        SocketClient.INSTANCE.setLastRecvPacketTime(System.currentTimeMillis());
         EventBus.getDefault().post(new Events.ReceivedEvent(msg));
     }
-
 }
